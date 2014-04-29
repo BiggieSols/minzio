471955b2d992060c0f06753c59cf2931c991fd49
Sol <solomon.garger@gmail.com>
2014-04-29 12:20:14 -0500

    switch database to postgres

--- Gemfile
+++ Gemfile
@@ -24,6 +24,7 @@ gem 'jquery-rails'
 
 gem 'omniauth'
 gem 'omniauth-linkedin'
+gem 'pg'
 
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
   attr_accessible :description, :email, :first_name, :headling, :industry, :last_name, :provider, :pub_profile, :uid
+
+  def self.from_omniauth(auth_hash)
+    user = User.where(uid: auth_hash.uid).first_or_initialize
+  end
 end
