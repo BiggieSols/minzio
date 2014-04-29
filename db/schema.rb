d5b07f7142d591bfa750dd5e64418257dce0f005
Sol <solomon.garger@gmail.com>
2014-04-29 15:50:29 -0500

    run migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,86 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140429170048) do
+ActiveRecord::Schema.define(:version => 20140429205000) do
+
+  create_table "answers", :force => true do |t|
+    t.text     "body"
+    t.string   "score"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "outcomes", :force => true do |t|
+    t.string   "title_string"
+    t.text     "details"
+    t.datetime "created_at",   :null => false
+    t.datetime "updated_at",   :null => false
+  end
+
+  create_table "question_answers", :force => true do |t|
+    t.integer  "question_id"
+    t.integer  "answer_id"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
+
+  add_index "question_answers", ["answer_id"], :name => "index_question_answers_on_answer_id"
+  add_index "question_answers", ["question_id"], :name => "index_question_answers_on_question_id"
+
+  create_table "questions", :force => true do |t|
+    t.text     "body"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "test_outcomes", :force => true do |t|
+    t.integer  "test_id"
+    t.integer  "outcome_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "test_outcomes", ["outcome_id"], :name => "index_test_outcomes_on_outcome_id"
+  add_index "test_outcomes", ["test_id"], :name => "index_test_outcomes_on_test_id"
+
+  create_table "test_questions", :force => true do |t|
+    t.integer  "test_id"
+    t.integer  "question_id"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
+
+  add_index "test_questions", ["question_id"], :name => "index_test_questions_on_question_id"
+  add_index "test_questions", ["test_id"], :name => "index_test_questions_on_test_id"
+
+  create_table "tests", :force => true do |t|
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
+
+  create_table "user_answers", :force => true do |t|
+    t.integer  "user_id"
+    t.integer  "answer_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "user_answers", ["answer_id"], :name => "index_user_answers_on_answer_id"
+  add_index "user_answers", ["user_id"], :name => "index_user_answers_on_user_id"
+
+  create_table "user_tests", :force => true do |t|
+    t.integer  "user_id"
+    t.integer  "test_id"
+    t.boolean  "started"
+    t.boolean  "finished"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "user_tests", ["test_id"], :name => "index_user_tests_on_test_id"
+  add_index "user_tests", ["user_id"], :name => "index_user_tests_on_user_id"
 
   create_table "users", :force => true do |t|
     t.string   "name"
