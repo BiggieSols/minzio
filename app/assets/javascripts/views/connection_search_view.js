03c730a16cc3e649f74161c948f698e176ed890b
Sol <solomon.garger@gmail.com>
2014-05-13 15:36:12 -0700

    can add members to groups with autocomplete

--- app/assets/javascripts/views/connection_search_view.js
+++ app/assets/javascripts/views/connection_search_view.js
@@ -1,10 +1,40 @@
 TeamProfile.Views.ConnectionSearchView = Backbone.View.extend({
   tagName: "span",
 
-  template: JST['misc/friend_search'],
+  template: JST['misc/connection_search'],
 
   // needs to be overridden in subclasses
   initialize: function() {
+    this.placeholder = "Add a group member...";
+    this.width = "180px";
+  },
+
+  dropdownFormat: function(state) {
+    return "<div class='row'><div class='col-xs-4'><img class='lazy friend-dropdown' src='" + state.url + "' class='dropdown-img'/></div><div class='col-xs-8 user-name-dropdown'>" + state.text  + "</div></div>";
+  },
+
+
+  _formAction: function(params) {
+    // console.log("form is taking an action!");
+    // params.item_id = $(event.currentTarget).closest(".item").data("id");
+    params.group_id = $('.group-container').data("id");
+    console.log(params);
+    var groupMembership = new TeamProfile.Models.GroupMember(params);
+
+    var that = this;
+    groupMembership.save({}, {
+      success: function() {
+        console.log("group membership saved!");
+        that.model.fetch();
+      }
+    });
+    // rec = new GiftMe.Models.UserItemRecommendation(params);
+    // rec.save();
+
+    // userName = this.$('.select2-search-choice div').text();
+    // GiftMe.dispatcher.trigger("recommendation", {userName: userName});
+
+    this._clearForm();
   },
 
   render: function() {
@@ -17,10 +47,6 @@ TeamProfile.Views.ConnectionSearchView = Backbone.View.extend({
     return this;
   },
 
-  // override for various sizes
-  dropdownFormat: function(state) {
-  },
-
   chosenItemFormat: function(state) {
     return "<span data-id='" + state.id +"'/>" + state.text + "</span>";
   },
@@ -30,17 +56,13 @@ TeamProfile.Views.ConnectionSearchView = Backbone.View.extend({
     this.$('.select2-input').focus();
   },
 
-  // needs to be overridden in subclasses
-  _formAction: function(params) {
-  },
-
   _generateSelect2Data: function() {
     var data = [];
-    GiftMe.users.models.forEach(function(user) {
+    TeamProfile.currentUser.get("connections").models.forEach(function(user) {
       data.push({
         id: user.id,
         text: user.escape("name"),
-        url: user.escape("small_picture_url")
+        url: user.escape("image_url")
       });
     });
     return {results: data};
@@ -48,7 +70,7 @@ TeamProfile.Views.ConnectionSearchView = Backbone.View.extend({
 
   _identifyFieldChanges: function() {
     var that = this;
-    this.$('.friend-selector').on("change", function(event) {
+    this.$('.connection-selector').on("change", function(event) {
       params = {};
       params.user_id = that.$('.select2-search-choice div span').data("id");
       that._formAction(params);
@@ -56,7 +78,7 @@ TeamProfile.Views.ConnectionSearchView = Backbone.View.extend({
   },
 
   _initalizeSelect2: function() {
-    this.$('.friend-selector').select2({
+    this.$('.connection-selector').select2({
       data: this._generateSelect2Data(),
       minimumInputLength: 3,
       placeholder: this.placeholder,
--- app/assets/javascripts/views/group_view.js
+++ app/assets/javascripts/views/group_view.js
@@ -1,12 +1,23 @@
 TeamProfile.Views.GroupView = Backbone.View.extend({
   template: JST['groups/show'],
+
+  initialize: function() {
+    this.listenTo(this.model, "sync", this.render);
+  },
   
   render: function() {
-    console.log("model is below");
-    console.log(this.model);
-    group = this.model
+    // console.log("model is below");
+    // console.log(this.model);
+    // group = this.model
     var renderedContent = this.template({group: this.model});
     this.$el.html(renderedContent);
+    this._renderConnectionSearch();
     return this;
-  }
+  },
+
+  _renderConnectionSearch: function() {
+    var connectionSearchView = new TeamProfile.Views.ConnectionSearchView({model: this.model});
+    this.$('#add-connections').html(connectionSearchView.render().$el);
+    return this;
+  },
 });
