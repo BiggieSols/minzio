7cc2feb15351a37c636fbe7b2d7dc0f1703dccf3
Sol <solomon.garger@gmail.com>
2014-05-20 22:34:56 -0700

    style improvements and randomization for quiz

--- app/assets/javascripts/models/group_member.js
+++ app/assets/javascripts/models/group_member.js
@@ -1,5 +1,32 @@
 TeamProfile.Models.GroupMember = Backbone.Model.extend({
   urlRoot: '/group_members',
+  collection: TeamProfile.Collections.GroupMembers,
+
+  destroy: function (options) {
+    options = options ? _.clone(options) : {};
+    var model = this;
+    var success = options.success;
+
+    var destroy = function() {
+      model.trigger('destroy', model, model.collection, options);
+    };
+
+    // options.success = function(resp) {
+    //   if (options.wait || model.isNew()) destroy();
+    //   if (success) success(model, resp, options);
+      // if (!model.isNew()) model.trigger('sync', model, resp, options);
+    // };
+
+    // if (this.isNew()) {
+    //   options.success();
+    //   return false;
+    // }
+    // wrapError(this, options);
+
+    var xhr = this.sync('delete', this, options);
+    if (!options.wait) destroy();
+    return xhr;
+  }
 
   // methodToUrl: {
   //   "destroy":"/group_member",
--- app/views/quiz/show.json.jbuilder
+++ app/views/quiz/show.json.jbuilder
@@ -1,9 +1,9 @@
 json.(@quiz, :title, :description)
 json.questions do
-  json.array!(@quiz.questions) do |question|
+  json.array!(@quiz.questions.shuffle) do |question|
     json.(question, :body, :id)
     json.answers do
-      json.array!(question.answers) do |answer|
+      json.array!(question.answers.shuffle) do |answer|
         json.(answer, :body, :id)
       end
     end
