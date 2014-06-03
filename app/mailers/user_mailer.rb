3a0ab5d813197cac0d3d3c41191aa50c04d0c942
Sol <solomon.garger@gmail.com>
2014-06-03 13:59:42 -0700

    send welcome emails

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,12 +1,11 @@
 class UserMailer < ActionMailer::Base
   default from: "\"The Minzio Team\" <sol@minzio.com>"
   default url:  "http://www.minzio.com"
-  "\"MyCompany\" <info@mycompany.com>"
 
   def welcome_email(user)
     @user           = user
     @url            = 'http://www.minzio.com'
-    mail(to: user.email, subject: 'Thanks for joining Minzio. Now what?')
+    mail(to: @user.email, subject: 'Thanks for joining Minzio. Now what?')
   end
 
   def group_invitation(params = {from_user: nil, to_user: nil, group: nil})
--- config/environment.rb
+++ config/environment.rb
@@ -6,7 +6,7 @@ Teamprofile::Application.initialize!
 
 
 # only send real emails in production; use Mandrill
-if Rails.env.production?
+if Rails.env.production? #|| Rails.env.development?
   ActionMailer::Base.smtp_settings = {
     :address              => "smtp.mandrillapp.com",
     :port                 => 25, # ports 587 and 2525 are also supported with STARTTLS
@@ -14,7 +14,7 @@ if Rails.env.production?
     :user_name            => ENV["MANDRILL_USERNAME"],
     :password             => ENV["MANDRILL_PASSWORD"], # SMTP password is any valid API key
     :authentication       => 'login', # Mandrill supports 'plain' or 'login'
-    :domain               => 'teamglide.com', # your domain to identify your server when connecting
+    :domain               => 'minzio.com', # your domain to identify your server when connecting
   }
   ActionMailer::Base.delivery_method ||= :smtp
 elsif Rails.env.development?
