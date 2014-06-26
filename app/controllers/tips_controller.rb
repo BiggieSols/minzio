9f41a7183d9cff7d4184d744c34f21731afd9aaa
Sol <solomon.garger@gmail.com>
2014-06-26 15:16:51 -0700

    security improvement for tip editing

--- app/controllers/tips_controller.rb
+++ app/controllers/tips_controller.rb
@@ -24,8 +24,11 @@ class TipsController < ApplicationController
 
   def update
     @tip = Tip.find(params[:id])
-    @tip.update_attributes(text: params[:text])
-    @tip.tip_votes.destroy_all
+
+    if [@tip.author_id, @tip.custom_personality.user_id].include? current_user.id
+      @tip.update_attributes(text: params[:text])
+      @tip.tip_votes.destroy_all
+    end
     # TipVote.create(vote_value: 1, tip_id: @tip.id, user_id: current_user.id)
     # @tip.reload
     render 'show.json.jbuilder'
