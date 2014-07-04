fb19a109ec1605af68f80ae4214aba4d1da3b0ec
Sol <solomon.garger@gmail.com>
2014-07-03 22:52:02 -0700

    set up company pages

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -2,14 +2,15 @@ class CompaniesController < ApplicationController
   skip_before_filter :require_login
 
   def index
-    @companies = User.select("company, count(*)")
-                    .group("company")
+    companies_query = User.select("company_id, count(*)")
+                    .group("company_id")
                     .order("count(*) desc")
                     .limit(100)
+    @companies = Company.find(companies_query.map(&:company_id))
   end
 
   def show
-    company = params[:company].gsub("_", " ")
-    users = User.where(company: company)
+    company = Company.find(params[:id])
+    @users = company.users
   end
 end
